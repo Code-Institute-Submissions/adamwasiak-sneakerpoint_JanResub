5491af7614a449e7a05c6105b506d9e43869a77e
adamwasiak <adam.wasiak82@gmail.com>
2022-10-09 15:30:44 +0000

    Add product management deletion logic

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile. """
@@ -15,7 +16,7 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            message.error(request, 'Update has failed.Please ensure form is filled correctly')
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -28,6 +29,7 @@ def profile(request):
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
