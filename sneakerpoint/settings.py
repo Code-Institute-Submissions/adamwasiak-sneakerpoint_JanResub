a1eecbe3d25c8ea7f9d386ac255ce93f43db8185
adamwasiak <adam.wasiak82@gmail.com>
2022-10-15 16:10:08 +0000

    remove secret key

--- sneakerpoint/settings.py
+++ sneakerpoint/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xy9)hz)4)+-p!sc$356$!e#t1+65-(y#0*8f)dbib60yq8!=8#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sneakerpoint.herokuapp.com', 'localhost']
 
