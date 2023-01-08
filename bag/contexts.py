bcbd13ee24b75a7c84bc794a33899cd1fde38906
adamwasiak <adam.wasiak82@gmail.com>
2023-01-08 19:31:54 +0000

    Update debug status for code release deployment

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -13,7 +14,7 @@ def bag_contents(request):
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data  * product.price
+            total += item_data * product.price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
